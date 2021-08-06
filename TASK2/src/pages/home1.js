9aeb839b6b0a058cbf95a3b91c2757dc2867b676
Yash Bobade <88370078+Yash-2001-17@users.noreply.github.com>
2021-08-06 20:13:57 +0530

    Add files via upload

---
+++ TASK2/src/pages/home1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home1 = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}
+        >
+            <h1>Welcome to Lets Grow More Internship</h1>
+        </div>
+    );
+};
+
+export default Home1;
